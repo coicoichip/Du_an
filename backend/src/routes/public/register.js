b4512fc6d8d4e0f3ea3f6c43510c3b217bd80cfb
hieunt <hieunt@kikaitech.io>
2020-12-19 17:12:46 +0700

    fix register

--- backend/src/routes/public/register.js
+++ backend/src/routes/public/register.js
@@ -5,22 +5,22 @@ const { handleAPIResponse } = require('../../common/handleAPIResponse');
 
 router.route('/api/register')
   .post(async (req, res, next) => {
-    const { email, password, position, name, phone, address, avatar } = req.body;
+    const { email, password, position, name, phone, address } = req.body;
     if (!email || !password) return handleAPIResponse(res, 400, 'email && password required');
     if (!['customer', 'owner'].includes(position)) return handleAPIResponse(res, 400, 'positon invalid');
     try {
       //
       const check_email = await knex('users').first().where({ email });
       if (check_email) return handleAPIResponse(res, 400, 'email exist');
-      const [user] = await knex('users').insert({
+      let [user] = await knex('users').insert({
         email,
         password,
         name,
         phone,
         address,
-        avatar,
         position,
-      }).returning('*');
+      });
+      user = await knex('users').first().where({ email });
       if (!user) return handleAPIResponse(res, 500, 'internal server error');
       Object.assign(req.session, user);
       return handleAPIResponse(res, 200);
