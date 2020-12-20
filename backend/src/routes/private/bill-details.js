09d43881010321a44c51508c66008795ff79ac96
hieunt <hieunt@kikaitech.io>
2020-12-20 23:37:35 +0700

    fix

--- backend/src/routes/private/bill-details.js
+++ backend/src/routes/private/bill-details.js
@@ -25,6 +25,7 @@ router.route('/api/restaurants/:restaurant_id/bills/:bill_id')
       next(e);
     }
   }).put(validateOwner, async (req, res, next) => {
+    const { user_id } = req.session;
     const { restaurant_id, bill_id } = req.params;
     const { status } = req.body;
     if (!restaurant_id || !bill_id) return handleAPIResponse(res, 400, 'restaurant_id && bill_id required');
@@ -35,8 +36,9 @@ router.route('/api/restaurants/:restaurant_id/bills/:bill_id')
         knex('restaurants').first().where({ id: restaurant_id }),
         knex('bills').first().where({ restaurant_id, id: bill_id }),
       ]);
-      if (!restaurant) return handleAPIResponse(res, 404, 'restaurant_id not exist');
       if (!bill) return handleAPIResponse(res, 404, 'bill_id not exist');
+      if (!restaurant) return handleAPIResponse(res, 404, 'restaurant_id not exist');
+      if (user_id !== restaurant.manager_id) return handleAPIResponse(res, 403, 'forbidden');
 
       await knex('bills').update({ status }).where({ id: bill_id });
       const bill1 = await knex('bills').first().where({ id: bill_id });
@@ -57,7 +59,7 @@ router.route('/api/restaurants/:restaurant_id/bills/:bill_id')
       ]);
       if (!restaurant) return handleAPIResponse(res, 404, 'restaurant_id not exist');
       if (!bill) return handleAPIResponse(res, 404, 'bill_id not exist');
-      if (restaurant.manager_id !== user_id && bill.recipient_id !== user_id && position !== 'admin') return handleAPIResponse(res, 403, 'forbidden');
+      if (restaurant.manager_id !== user_id && position !== 'admin') return handleAPIResponse(res, 403, 'forbidden');
 
       await Promise.all([
         knex('bills').delete().where({ restaurant_id, id: bill_id }),
