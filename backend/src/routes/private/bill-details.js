c3d6062d08dc408b0192420d997e65faa28675f1
hieunt <hieunt@kikaitech.io>
2020-12-20 18:53:29 +0700

    fix put bill-details

--- backend/src/routes/private/bill-details.js
+++ backend/src/routes/private/bill-details.js
@@ -25,10 +25,10 @@ router.route('/api/restaurants/:restaurant_id/bills/:bill_id')
       next(e);
     }
   }).put(validateOwner, async (req, res, next) => {
-    const { user_id, position } = req.session;
     const { restaurant_id, bill_id } = req.params;
-    const { status, ship_price, bill_detail } = req.body;
+    const { status } = req.body;
     if (!restaurant_id || !bill_id) return handleAPIResponse(res, 400, 'restaurant_id && bill_id required');
+    if (![0, 1].includes(status)) return handleAPIResponse(res, 400, '1 <= status <= 5');
 
     try {
       const [restaurant, bill] = await Promise.all([
@@ -37,39 +37,11 @@ router.route('/api/restaurants/:restaurant_id/bills/:bill_id')
       ]);
       if (!restaurant) return handleAPIResponse(res, 404, 'restaurant_id not exist');
       if (!bill) return handleAPIResponse(res, 404, 'bill_id not exist');
-      if (restaurant.manager_id !== user_id && bill.recipient_id !== user_id && position !== 'admin') return handleAPIResponse(res, 403, 'forbidden');
-      if (position === 'customer' && ([0, 1].includes(status) || ship_price)) return handleAPIResponse(res, 403, 'status && customer && ship_price forbidden');
 
-      await knex('bill_detail').delete().where({ bill_id });
-      const update_data = {};
-      ['status', 'ship_price', 'note', 'recipient_id', 'recipient_name', 'recipient_email', 'recipient_phone', 'recipient_address'].forEach(field => {
-        if (req.body[field]) update_data[field] = req.body[field];
-      });
+      await knex('bills').update({ status }).where({ bill_id });
+      const bill1 = await knex('bills').first().where({ bill_id });
 
-      let total = 0;
-      const foods = await Promise.all(bill_detail.map(({ food_id }) =>
-        knex('foods').first().where({ id: food_id }),
-      ));
-      const total_bill_details = bill_detail.map(({ food_id, quantity }, idx) => {
-        const amount = foods[idx].price * quantity;
-        total += amount;
-        return { bill_id: bill.id, food_id, quantity, amount };
-      });
-      Object.assign(update_data, { total });
-
-      await knex('bills')
-        .update(update_data)
-        .where({ id: bill_id });
-      await Promise.all(total_bill_details.map(bill_detail1 =>
-        knex('bill_detail').insert(bill_detail1),
-      ));
-
-      const bill1 = await knex('bills').first().where({ bill_code: bill.bill_code });
-      const bill_detail1 = await knex('bill_detail').where({ bill_id: bill.id });
-
-      const result = { ...bill1, bill_detail1 };
-
-      return handleAPIResponse(res, 200, result);
+      return handleAPIResponse(res, 200, bill1);
     } catch (e) {
       next(e);
     }
